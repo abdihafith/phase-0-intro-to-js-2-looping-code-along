a607af40d10b2bfe86bcf894b9476d1070aa23ba
Abdihafith <abdyhafith@gmail.com>
2024-03-24 22:08:18 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names = ["Ada", "Brendan", "Ali"]) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(startingNumber) {
+      for (let i = startingNumber; i >=0; i--){
+          console.log(i);
+      }
+  }
